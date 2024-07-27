44cc2022d8a6bd4cc622ac8e59dee3b35033613b
manichand <mani.chand01@gmail.com>
2024-07-27 10:49:23 +0530

    Updated for api call

--- src/main/java/com/app/notes/controller/NoteController.java
+++ src/main/java/com/app/notes/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.app.notes.controller;
 import com.app.notes.model.Note;
 import com.app.notes.service.NoteService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 // import java.util.Optional;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class NoteController {
